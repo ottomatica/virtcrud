5c5efe5b20e669404f44abe26991e5e1b8c0dd80
Chris Parnin <chris.parnin@gmail.com>
2022-01-05 21:30:50 -0500

    rm irrelevant code.

--- providers/vf/index.js
+++ providers/vf/index.js
@@ -3,19 +3,16 @@ const Shell = require("./shell");
 class VirtualizationFramework {
 
     async create(name, options) {
-        return await Shell.StartVM(name);        
+        return await Shell.StartVM(name);
     }
 
     async stop(name, options) {
-        return await PowerShellCommandlets.StopVM(name);
     }
 
     async start(name, options) {
-        return await PowerShellCommandlets.Start(name);
     }
 
     async status(name, options) {
-        return await PowerShellCommandlets.GetVMState(name);s
     }
 
 
